45aaed48bb89b1938102306a595acf641d504c24
shubhamyadav97660@gmail.com <neeta6854@gmail.com>
2023-12-10 20:18:11 +0530

    a new file CustomeThemeSwitcher.jsx added and previous codes are also modified

--- src/ThemeContext.jsx
+++ src/ThemeContext.jsx
@@ -1,19 +1,39 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext } from "react";
 
 const ThemeContext = createContext();
+const CustomThemeContext = createContext();
 
 export const useTheme = () => {
   return useContext(ThemeContext);
 };
 
+export const useContextTheme = () => {
+  return useContext(CustomThemeContext);
+};
+
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
+  const [customeTheme, setCustomeTheme] = useState({
+    light: { background: "#ffffff", text: "#000000" },
+    dark: { background: "#333333", text: "#ffffff" },
+  });
 
   const toggleTheme = () => {
-    setTheme(prevTheme => (prevTheme === 'light' ? 'dark':'light'));
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
-  return <ThemeContext.Provider value={{theme, toggleTheme}}>
-    {children}
-  </ThemeContext.Provider>
-}
+  const updateCustomTheme = (newTheme) => {
+    setCustomeTheme((prevCustomeTheme) => ({
+      ...prevCustomeTheme,
+      [theme]: newTheme,
+    }));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <CustomThemeContext.Provider value={{ customeTheme, updateCustomTheme}}>
+      {children}
+      </CustomThemeContext.Provider>
+    </ThemeContext.Provider>
+  );
+};
